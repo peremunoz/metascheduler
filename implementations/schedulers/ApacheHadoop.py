b03c0ccf708cc6deba65bb7e1af42883c4ddf7c9
peremunoz <peremunozfiguerol33@gmail.com>
2024-03-27 21:56:43 +0100

    Create schedulers specific classes

---
+++ implementations/schedulers/ApacheHadoop.py
@@ -0,0 +1,10 @@
+from interfaces.Scheduler import Scheduler
+
+
+class ApacheHadoop(Scheduler):
+    """
+    Apache Hadoop scheduler.
+    
+    """
+    def __str__(self):
+        return "Apache Hadoop Scheduler"
---
+++ implementations/schedulers/SGE.py
@@ -0,0 +1,10 @@
+from interfaces.Scheduler import Scheduler
+
+
+class SGE(Scheduler):
+    """
+    SGE scheduler.
+    
+    """
+    def __str__(self):
+        return "SGE Scheduler"
