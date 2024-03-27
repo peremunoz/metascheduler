052bdd5774ee8d4d665fc14e8c563d93169d70d9
peremunoz <peremunozfiguerol33@gmail.com>
2024-03-27 21:57:56 +0100

    Save point, something done

---
+++ main.py
@@ -0,0 +1,35 @@
+from interfaces.Cluster import Cluster
+from ui.ClusterList import ask_for_cluster
+from utils.ClusterFactory import get_cluster
+from utils.ColorPrinter import print_color
+
+def main():
+    """
+    Main function of the program.
+
+    """
+    cluster: Cluster = clusterSetup()
+
+def clusterSetup() -> Cluster:
+    """
+    Initial setup of the cluster.
+
+    """
+    # Ask the user to select a cluster system
+    cluster_system = ask_for_cluster()
+    print("Selected cluster system: ", end='')
+    print_color(cluster_system, "blue")
+
+    # Get the cluster object
+    cluster = get_cluster(cluster_system)
+
+    # Setup the cluster
+    cluster.setup()
+
+    print(cluster)
+
+    return cluster
+    
+
+if __name__ == '__main__':
+    main()
