96d70e9bdc3f6080baf43f20bee6de1cb38db22d
peremunoz <peremunozfiguerol33@gmail.com>
2024-03-27 21:57:38 +0100

    Create helper to print in colors easily

---
+++ utils/ColorPrinter.py
@@ -0,0 +1,10 @@
+from rich.console import Console
+from rich.text import Text
+
+def print_color(text: str, color: str, end='\n') -> None:
+    """
+    Prints the given text with the given color.
+
+    """
+    console = Console()
+    console.print(Text(text, style=f"bold {color}"), end=end)
