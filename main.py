a54fa507e53dffa2e060b35e51d49164b07158fb
peremunoz <peremunozfiguerol33@gmail.com>
2024-04-09 21:53:13 +0200

    Refactor naming and some prints

--- main.py
+++ main.py
@@ -1,32 +1,30 @@
 from interfaces.Cluster import Cluster
 from ui.ClusterList import ask_for_cluster
 from utils.ClusterFactory import get_cluster
-from utils.ConsoleHelper import print_color
+from utils.ConsoleHelper import print_cluster_selection
 
 def main():
     """
     Main function of the program.
 
     """
-    cluster: Cluster = clusterSetup()
+    cluster: Cluster = cluster_setup()
+    cluster.setup_schedulers()
 
-def clusterSetup() -> Cluster:
+def cluster_setup() -> Cluster:
     """
     Initial setup of the cluster.
 
     """
     # Ask the user to select a cluster system
-    cluster_system = ask_for_cluster()
-    print("Selected cluster system: ", end='')
-    print_color(cluster_system, "blue")
+    cluster_system: str = ask_for_cluster()
 
     # Get the cluster object
-    cluster = get_cluster(cluster_system)
+    cluster: Cluster = get_cluster(cluster_system)
+    print_cluster_selection(cluster)
 
     # Setup the cluster
     cluster.setup()
-
-    print(cluster)
 
     return cluster
     
