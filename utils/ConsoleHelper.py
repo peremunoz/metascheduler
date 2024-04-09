6d6caa37725550f75f45e362c03cbbc193f75e14
peremunoz <peremunozfiguerol33@gmail.com>
2024-04-09 20:25:30 +0200

    Implement methods to print selected cluster and selected schedulers

--- utils/ConsoleHelper.py
+++ utils/ConsoleHelper.py
@@ -1,6 +1,8 @@
 from rich.console import Console
 from rich.text import Text
 
+from interfaces.Cluster import Cluster
+
 def print_color(text: str, color: str, end='\n') -> None:
     """
     Prints the given text with the given color.
@@ -16,3 +18,24 @@ def clear_console() -> None:
     """
     console = Console()
     console.clear()
+
+def print_cluster_selection(cluster: Cluster) -> None:
+    """
+    Prints the selected cluster.
+
+    """
+    print("Selected cluster system: ", end='')
+    print_color(cluster.__str__(), "blue")
+
+def print_scheduler_selection(cluster: Cluster) -> None:
+    """
+    Prints the selected schedulers.
+    
+    """
+    print("Selected schedulers: ", end='')
+    for i, scheduler in enumerate(cluster.get_schedulers()):
+        if i == len(cluster.get_schedulers()) - 1:
+            print_color(scheduler.__str__(), "blue", '')
+            print()
+        else:
+            print_color(scheduler.__str__(), "blue", ', ')
