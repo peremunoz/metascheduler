aafb5e54e3e0617f2b0466155819a867983d4258
peremunoz <peremunozfiguerol33@gmail.com>
2024-04-18 21:42:02 +0200

    Implement setup from file and manual setup flow.
    - Also remove old and unused methods

--- interfaces/Cluster.py
+++ interfaces/Cluster.py
@@ -1,5 +1,13 @@
 from typing import List
+from pathlib import Path
+import json
+
+import typer
 from interfaces.Scheduler import Scheduler
+from interfaces.Node import Node
+from ui.AvailableSchedulers import print_available_schedulers
+from utils.ConsoleHelper import print_node_setup, print_scheduler_setup
+from utils.SchedulerFactory import get_scheduler
 
 
 class Cluster:
@@ -7,23 +15,78 @@ class Cluster:
     Interface for a cluster.
 
     """
+    nodes: List[Node]
     schedulers: List[Scheduler]
-    master: str
 
-    def setup():
+    def setup_from_file(self, file_path: Path):
         """
-        Sets up the cluster.
+        Sets up the cluster from a file.
 
         """
-        raise NotImplementedError("Method not implemented")
+        json_config = json.loads(file_path.read_text())
+
+        nodes = json_config['cluster']['nodes']
+        nodes_list = []
+        for node in nodes:
+            nodes_list.append(Node(node['ip'], node['port'], node['user'], node['password']))
+        self._set_nodes(nodes_list)
+
+        schedulers = json_config['cluster']['schedulers']
+        schedulers_list = []
+        for scheduler in schedulers:
+            schedulers_list.append(get_scheduler(scheduler['name']))
+        self._set_schedulers(schedulers_list)
+
+        print_node_setup(self)
+        print()
+        print_scheduler_setup(self)
+
+    def manual_setup(self):
+        """
+        Sets up the cluster manually, via the console.
+
+        """
+        self._setup_schedulers()
+        self._setup_nodes()
     
-    def setup_schedulers():
+    def _setup_schedulers(self):
         """
         Sets up the schedulers of the cluster.
         
         """
-        raise NotImplementedError("Method not implemented")
+        all_scheduler_names_str: List[str] = print_available_schedulers()
+        schedulers_selected_options: List[int] = typer.prompt("Enter the options of the schedulers you want to use, separated by commas: ").split(",")
+        scheduler_names_str: List[str] = []
+        for option in schedulers_selected_options:
+            scheduler_names_str.append(all_scheduler_names_str[int(option)])
+        schedulers_list: List[Scheduler] = []
+        for scheduler in scheduler_names_str:
+            schedulers_list.append(get_scheduler(scheduler))
+        self.set_schedulers(schedulers_list)
+        print_scheduler_setup(self)
 
+    def _setup_nodes(self):
+        """
+        Sets up the nodes of the cluster.
+        
+        """
+        nodes_number: int = int(input("Enter the number of nodes: "))
+        nodes_list: List[Node] = []
+        for i in range(nodes_number):
+            ssh_ip: str = input(f"Enter the IP of node {i + 1}: ").strip()
+            ssh_port: str = input(f"Enter the port of node {i + 1} [22]: ").strip()
+            if ssh_port == "":
+                ssh_port = 22
+            ssh_user: str = input(f"Enter the user of node {i + 1} [root]: ").strip()
+            if ssh_user == "":
+                ssh_user = "root"
+            ssh_password: str = input(f"Enter the password of node {i + 1} [root]: ").strip()
+            if ssh_password == "":
+                ssh_password = "root"
+            nodes_list.append(Node(ssh_ip, int(ssh_port), ssh_user, ssh_password))
+        self._set_nodes(nodes_list)
+        print_node_setup(self)
+    
     def connect(self):
         """
         Connects to the cluster.
@@ -59,30 +122,23 @@ class Cluster:
         """
         return self.schedulers
     
-    def set_schedulers(self, schedulers: List[Scheduler]):
+    def _set_schedulers(self, schedulers: List[Scheduler]):
         """
         Sets the schedulers of the cluster.
         
         """
         self.schedulers = schedulers
     
-    def remove_scheduler(self, scheduler: Scheduler):
+    def get_nodes(self) -> List[Node]:
         """
-        Removes a scheduler from the cluster.
+        Gets the nodes of the cluster.
         
         """
-        self.schedulers.remove(scheduler)
+        return self.nodes
     
-    def get_master(self) -> str:
+    def _set_nodes(self, nodes: List[Node]):
         """
-        Gets the master of the cluster.
+        Sets the nodes of the cluster.
         
         """
-        return self.master
-    
-    def set_master(self, master: str):
-        """
-        Sets the master of the cluster.
-        
-        """
-        self.master = master
+        self.nodes = nodes
