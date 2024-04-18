d5ac15253eeb58949ed7591a1cb56a08a13d9cdb
peremunoz <peremunozfiguerol33@gmail.com>
2024-04-18 21:41:04 +0200

    Remove set master from Scheduler interface

--- interfaces/Scheduler.py
+++ interfaces/Scheduler.py
@@ -3,11 +3,3 @@ class Scheduler():
     Interface for a scheduler.
 
     """
-    master: str
-
-    def set_master(self, master: str):
-        """
-        Sets the master of the scheduler.
-
-        """
-        self.master = master
