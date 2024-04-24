1ad4eda3c3571a9b3ed16948bc70a0bc6d69190b
peremunoz <peremunozfiguerol33@gmail.com>
2024-04-25 00:04:59 +0200

    Define config file as a parameter and load the config singleton

--- api/api.py
+++ api/api.py
@@ -1,6 +1,8 @@
+from pathlib import Path
 from fastapi import FastAPI
 import typer
 from typing_extensions import Annotated
+from config.config import AppConfig
 
 
 app = FastAPI()
@@ -12,8 +14,20 @@ def read_status():
     return {"status": "ok"}
 
 
-def main(host: Annotated[str, typer.Option(help='Host to bind to')] = '0.0.0.0',
-         port: Annotated[int, typer.Option(help='Port to bind to')] = 8000):
+def main(
+        config_file: Annotated[Path, typer.Argument(
+            help="The config file to read the cluster values from.",
+            exists=True,
+            file_okay=True,
+            dir_okay=False,
+            writable=False,
+            readable=True,
+            resolve_path=True,
+        )],
+        host: Annotated[str, typer.Option(help='Host to bind to')] = '0.0.0.0',
+        port: Annotated[int, typer.Option(help='Port to bind to')] = 8000
+):
+    AppConfig(config_file)
     import uvicorn
     uvicorn.run(app, host=host, port=port)
 
