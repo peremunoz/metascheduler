48d8c5f480d232f2dbb92678dfdc545710521029
peremunoz <peremunozfiguerol33@gmail.com>
2024-04-27 19:35:22 +0200

    Add id attribute and is_alive method

--- api/interfaces/Node.py
+++ api/interfaces/Node.py
@@ -1,18 +1,23 @@
+from icmplib import ping
+
+
 class Node:
     """
     Interface for a node.
 
     """
+    id: int
     ip: str
     port: int
     ssh_user: str
     ssh_password: str
 
-    def __init__(self, ip: str, port: int, ssh_user: str, ssh_password: str) -> None:
+    def __init__(self, id: int, ip: str, port: int, ssh_user: str, ssh_password: str) -> None:
         """
         Constructor.
 
         """
+        self.id = id
         self.ip = ip
         self.port = port
         self.ssh_user = ssh_user
@@ -23,4 +28,15 @@ class Node:
         String representation of the node.
 
         """
-        return f"IP: {self.ssh_ip}, Port: {self.ssh_port}, User: {self.ssh_user}, Password: {self.ssh_password}"
+        return f"IP: {self.ip}, Port: {self.port}, User: {self.ssh_user}, Password: {self.ssh_password}"
+
+    def is_alive(self) -> bool:
+        """
+        [ROOT REQUIRED]
+        Check if the node is alive.
+
+        Returns:
+            bool: True if the node is alive, False otherwise.
+
+        """
+        return ping(self.ip, count=1).is_alive
