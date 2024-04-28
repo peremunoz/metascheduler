eeb7cdff228a4cf7375a2681f3a877442fcb3ffb
peremunoz <peremunozfiguerol33@gmail.com>
2024-04-28 18:54:45 +0200

    Add optional parameter to specify the database file

--- api/api.py
+++ api/api.py
@@ -1,43 +1,21 @@
 from pathlib import Path
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 import typer
 from typing_extensions import Annotated
 from config.config import AppConfig
+from routers import nodes, jobs
 
 
 app = FastAPI()
+
+app.include_router(nodes.router)
+app.include_router(jobs.router)
 
 
 @app.get("/")
 @app.get("/status")
 def read_status():
     return {"status": "running", "root": AppConfig().root}
-
-
-@app.get("/nodes")
-def read_nodes():
-    return [{"id": node.id, "ip": node.ip, "port": node.port, "ssh_user": node.ssh_user, "is_alive": node.is_alive()} for node in AppConfig().nodes]
-
-
-@app.get("/nodes/{node_id}")
-def read_node(node_id: int):
-    if node_id >= len(AppConfig().nodes):
-        raise HTTPException(status_code=404, detail="Node not found")
-
-    node = AppConfig().nodes[node_id]
-
-    return {
-        "id": node.id,
-        "ip": node.ip,
-        "port": node.port,
-        "ssh_user": node.ssh_user,
-        "is_alive": node.is_alive()
-    }
-
-
-@app.get("/master")
-def read_master_node():
-    return AppConfig().master_node
 
 
 def main(
@@ -50,10 +28,19 @@ def main(
             readable=True,
             resolve_path=True,
         )],
+        database_file: Annotated[Path, typer.Option(
+            help="The database file to store the job queue.",
+            exists=False,
+            file_okay=True,
+            dir_okay=False,
+            writable=True,
+            readable=False,
+            resolve_path=True,
+        )] = None,
         host: Annotated[str, typer.Option(help='Host to bind to')] = '0.0.0.0',
         port: Annotated[int, typer.Option(help='Port to bind to')] = 8000
 ):
-    AppConfig(config_file)
+    AppConfig(config_file, database_file)
     import uvicorn
     uvicorn.run(app, host=host, port=port)
 
