9a083f275aa788f7ec4848c5970a896afdd546c7
peremunoz <peremunozfiguerol33@gmail.com>
2024-04-28 18:52:49 +0200

    Move scheduler factory file

--- api/utils/SchedulerFactory.py
+++ api/utils/SchedulerFactory.py
@@ -1,13 +1,13 @@
 def get_scheduler(scheduler_name: str):
     """
-    Gets the scheduler object based on the selected scheduler name.
+    Gets the scheduler class based on the scheduler name.
 
     """
     if scheduler_name == "Apache Hadoop":
-        from implementations.schedulers.ApacheHadoop import ApacheHadoop
+        from classes.ApacheHadoop import ApacheHadoop
         return ApacheHadoop()
     elif scheduler_name == "SGE":
-        from implementations.schedulers.SGE import SGE
+        from classes.SGE import SGE
         return SGE()
     else:
         raise ValueError("Scheduler not implemented")
