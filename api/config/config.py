441091eee422d31a413c3dbf6d763eb56f1dfd58
peremunoz <peremunozfiguerol33@gmail.com>
2024-04-28 18:54:27 +0200

    Instantiate database helper in config file

--- api/config/config.py
+++ api/config/config.py
@@ -2,6 +2,9 @@ import os
 import json
 from pathlib import Path
 from typing import Any, List
+from utils.DatabaseHelper import DatabaseHelper
+from interfaces.Scheduler import Scheduler
+from utils.SchedulerFactory import get_scheduler
 from utils.Singleton import Singleton
 from interfaces.Node import Node
 
@@ -12,12 +15,15 @@ class AppConfig(metaclass=Singleton):
     root: bool
     nodes: List[Node]
     master_node: Node
+    schedulers: List[Scheduler]
 
-    def __init__(self, config_file: Path = None) -> None:
+    def __init__(self, config_file: Path = None, database_file: Path = None) -> None:
         if (config_file):
             self.root = os.geteuid() == 0
             self._load_config(config_file)
             self._load_nodes()
+            self._load_schedulers()
+            self._init_db(database_file)
         else:
             raise Exception(
                 "Config file not provided on first initialization.")
@@ -36,3 +42,14 @@ class AppConfig(metaclass=Singleton):
             node_id += 1
         self.nodes = nodes_list
         self.master_node = nodes_list[0]
+
+    def _load_schedulers(self) -> None:
+        schedulers = self._config['cluster']['schedulers']
+        schedulers_list: List[Scheduler] = []
+        for scheduler in schedulers:
+            scheduler_obj = get_scheduler(scheduler['name'])
+            schedulers_list.append(scheduler_obj)
+        self.schedulers = schedulers_list
+
+    def _init_db(self, database_file: Path) -> None:
+        DatabaseHelper(database_file)
