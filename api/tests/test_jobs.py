eb42155b8e1858371b002c0cda4cce2303530185
peremunoz <peremunozfiguerol33@gmail.com>
2024-05-01 21:28:38 +0200

    Add test cases for reading jobs by owner and individual job retrieval in test_jobs.py

--- api/tests/test_jobs.py
+++ api/tests/test_jobs.py
@@ -28,6 +28,7 @@ def test_create_job_one(client):
     assert response.status_code == 201
     assert response.json() == {"status": "success",
                                "message": "Job created successfully"}
+
     response = client.get("/jobs", params={"owner": "root"})
     assert response.status_code == 200
     assert len(response.json()) == 1
@@ -43,6 +44,7 @@ def test_create_job_many(client):
     assert response.status_code == 201
     response = client.post("/jobs", json=test_job_3)
     assert response.status_code == 201
+
     response = client.get("/jobs", params={"owner": "root"})
     assert response.status_code == 200
     assert len(response.json()) == 3
@@ -64,31 +66,58 @@ def test_create_job_invalid_queue(client):
     assert response.json() == {"detail": "Queue 3 not found"}
 
 
-def test_read_job_by_owner(client):
+def test_read_jobs_by_owner(client):
     response = client.post("/jobs", json=test_job_1)
     assert response.status_code == 201
     response = client.post("/jobs", json=test_job_2)
     assert response.status_code == 201
     response = client.post("/jobs", json=test_job_3)
     assert response.status_code == 201
+
     response = client.get("/jobs", params={"owner": "owner1"})
     assert response.status_code == 200
     assert len(response.json()) == 1
     assert response.json()[0]["name"] == test_job_1["name"]
     assert response.json()[0]["queue"] == test_job_1["queue"]
     assert response.json()[0]["owner"] == test_job_1["owner"]
+
     response = client.get("/jobs", params={"owner": "owner2"})
     assert response.status_code == 200
     assert len(response.json()) == 1
     assert response.json()[0]["name"] == test_job_2["name"]
     assert response.json()[0]["queue"] == test_job_2["queue"]
     assert response.json()[0]["owner"] == test_job_2["owner"]
+
     response = client.get("/jobs", params={"owner": "owner3"})
     assert response.status_code == 200
     assert len(response.json()) == 1
     assert response.json()[0]["name"] == test_job_3["name"]
     assert response.json()[0]["queue"] == test_job_3["queue"]
     assert response.json()[0]["owner"] == test_job_3["owner"]
+
     response = client.get("/jobs", params={"owner": "owner4"})
     assert response.status_code == 200
     assert response.json() == []
+
+
+def test_read_job(client):
+    response = client.post("/jobs", json=test_job_1)
+    assert response.status_code == 201
+
+    response = client.get("/jobs/1", params={"owner": "owner1"})
+    assert response.status_code == 200
+    assert response.json()["name"] == test_job_1["name"]
+    assert response.json()["queue"] == test_job_1["queue"]
+    assert response.json()["owner"] == test_job_1["owner"]
+
+    response = client.get("/jobs/1", params={"owner": "owner2"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Job not found"}
+
+    response = client.get("/jobs/2", params={"owner": "owner1"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Job not found"}
+
+    response = client.get("/jobs/2", params={"owner": "owner2"})
+    assert response.status_code == 404
+    assert response.json() == {"detail": "Job not found"}
