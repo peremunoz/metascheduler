53d30d11987875c53833ea05a72035ca6eef4d39
peremunoz <peremunozfiguerol33@gmail.com>
2024-05-01 19:18:11 +0200

    Add test case for reading master node

--- api/tests/test_nodes.py
+++ api/tests/test_nodes.py
@@ -18,3 +18,12 @@ def test_read_nodes():
         assert response.json()[i]['ip'] == test_nodes[i]['ip']
         assert response.json()[i]['port'] == test_nodes[i]['port']
         assert response.json()[i]['is_alive'] == False or True or None
+
+
+def test_read_master_node():
+    response = client.get("/nodes/master")
+    assert response.status_code == 200
+    assert response.json()['id'] == 0
+    assert response.json()['ip'] == test_nodes[0]['ip']
+    assert response.json()['port'] == test_nodes[0]['port']
+    assert response.json()['is_alive'] == False or True or None
