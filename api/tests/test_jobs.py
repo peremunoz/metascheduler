5b3caa4a9554da9e37f87fc2da8d4adb419f9390
peremunoz <peremunozfiguerol33@gmail.com>
2024-05-01 23:16:29 +0200

    Refactor test_read_jobs_by_status function in test_jobs.py to include additional test cases for filtering jobs by status

--- api/tests/test_jobs.py
+++ api/tests/test_jobs.py
@@ -1,16 +1,19 @@
-test_job_1 = {
+from api.constants.JobStatus import JobStatus
+from api.routers.jobs import PostJobModel
+
+test_job_1: PostJobModel = {
     "name": "job1",
     "queue": 1,
     "owner": "owner1"
 }
 
-test_job_2 = {
+test_job_2: PostJobModel = {
     "name": "job2",
     "queue": 1,
     "owner": "owner2"
 }
 
-test_job_3 = {
+test_job_3: PostJobModel = {
     "name": "job3",
     "queue": 2,
     "owner": "owner3"
@@ -96,6 +99,43 @@ def test_read_jobs_by_owner(client):
     assert response.json()[0]["owner"] == test_job_3["owner"]
 
     response = client.get("/jobs", params={"owner": "owner4"})
+    assert response.status_code == 200
+    assert response.json() == []
+
+
+def test_read_jobs_by_status(client):
+    response = client.post("/jobs", json=test_job_1)
+    assert response.status_code == 201
+    response = client.post("/jobs", json=test_job_2)
+    assert response.status_code == 201
+    response = client.post("/jobs", json=test_job_3)
+    assert response.status_code == 201
+
+    response = client.get(
+        "/jobs", params={"owner": "root", "status": JobStatus.QUEUED.value})
+    assert response.status_code == 200
+    assert len(response.json()) == 3
+    assert response.json()[0]["name"] == test_job_1["name"]
+    assert response.json()[0]["queue"] == test_job_1["queue"]
+    assert response.json()[0]["owner"] == test_job_1["owner"]
+    assert response.json()[1]["name"] == test_job_2["name"]
+    assert response.json()[1]["queue"] == test_job_2["queue"]
+    assert response.json()[1]["owner"] == test_job_2["owner"]
+    assert response.json()[2]["name"] == test_job_3["name"]
+    assert response.json()[2]["queue"] == test_job_3["queue"]
+    assert response.json()[2]["owner"] == test_job_3["owner"]
+
+    response = client.get(
+        "/jobs", params={"owner": "root", "status": JobStatus.RUNNING.value})
+    assert response.status_code == 200
+    assert response.json() == []
+
+    response = client.get(
+        "/jobs", params={"owner": "root", "status": "WRONG_STATUS"})
+    assert response.status_code == 422
+
+    response = client.get(
+        "/jobs", params={"owner": "root", "status": JobStatus.RUNNING.value})
     assert response.status_code == 200
     assert response.json() == []
 
