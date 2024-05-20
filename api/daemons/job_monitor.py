8d945ba52705b8e02395bc77a49edb3f40bac3c1
peremunoz <peremunozfiguerol33@gmail.com>
2024-05-20 19:51:55 +0200

    Write some logs to track better the daemon execution

--- api/daemons/job_monitor.py
+++ api/daemons/job_monitor.py
@@ -1,33 +1,55 @@
 import threading
 import time
 from api.utils.singleton import Singleton
+from rich import print
+
+
+def log(message):
+    ''' Log messages to the console '''
+    prefix = 'DAEMON:'.ljust(10)
+    print(f'[cyan]{prefix}[/cyan]{message}')
 
 
 class JobMonitorDaemon(metaclass=Singleton):
+    ''' The Job Monitor Daemon is responsible for monitoring the jobs in the database,
+    checking the scheduler queues, and making decisions based on the monitored jobs and queues.
+
+    The daemon runs in a separate thread and is started and stopped by the main application.
+
+    The Job Monitor Daemon is a Singleton class, meaning that only one instance of the class
+    can be created. This is useful because we only need one instance of the daemon running
+    in the application.
+    '''
+
+    metascheduler_queue = []
+
     def __init__(self):
         self._stop_event = threading.Event()
 
     def start(self):
+        ''' Start the daemon '''
+        log('Starting...')
         while not self._stop_event.is_set():
-            print(
-                f'Job Monitor Daemon is running... pid: {threading.get_ident()}')
             self._monitor_jobs()
             self._check_queues()
             self._make_decisions()
             time.sleep(5)
 
     def stop(self):
-        print(f'Stopping Job Monitor Daemon... pid: {threading.get_ident()}')
+        log(f'Shutting down...')
         self._stop_event.set()
 
     def _monitor_jobs(self):
-        print('Monitoring jobs...')
+        ''' Monitor the jobs in the database '''
+        log('Monitoring jobs...')
         pass
 
     def _check_queues(self):
-        print('Checking queues...')
+        ''' Check the scheduler queues '''
+        log('Checking queues...')
         pass
 
     def _make_decisions(self):
-        print('Making decisions...')
+        ''' Make decisions based on the monitored jobs and queues '''
+        log('Making decisions...')
         pass
