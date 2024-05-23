91ed5a91b554a179152960eaff363e01c9d58b5e
peremunoz <peremunozfiguerol33@gmail.com>
2024-05-23 18:45:11 +0200

    Define the structure to implement the get job list method in sge

--- api/classes/sge.py
+++ api/classes/sge.py
@@ -1,3 +1,5 @@
+from typing import List
+from api.interfaces.job import Job
 from api.interfaces.scheduler import Scheduler
 
 
@@ -13,3 +15,28 @@ class SGE(Scheduler):
 
     def __str__(self) -> str:
         return f'SGE Scheduler: {self.hostname}:{self.port}'
+
+    def get_job_list(self) -> List[Job]:
+        '''
+        Get the list of jobs from the SGE scheduler
+
+        '''
+        qstat = self._call_qstat()
+        jobs = self._parse_qstat(qstat)
+        return jobs
+
+    def _call_qstat(self) -> str:
+        '''
+        TODO:
+        Call the qstat command to get the list of jobs
+
+        '''
+        raise NotImplementedError
+
+    def _parse_qstat(self, qstat_output) -> List[Job]:
+        '''
+        TODO:
+        Parse the output of the qstat command
+
+        '''
+        raise NotImplementedError
