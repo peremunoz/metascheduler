9462e3fc0514e48842da6e27eaef3da9a0338a55
peremunoz <peremunozfiguerol33@gmail.com>
2024-05-23 18:40:00 +0200

    Implement get job list for apache hadoop

--- api/classes/apache_hadoop.py
+++ api/classes/apache_hadoop.py
@@ -1,3 +1,5 @@
+from typing import List
+from api.interfaces.job import Job
 from api.interfaces.scheduler import Scheduler
 
 
@@ -7,9 +9,21 @@ class ApacheHadoop(Scheduler):
 
     '''
 
+    running_job: Job
+
     def __init__(self) -> None:
         super().__init__()
         self.name = 'Apache Hadoop'
+        self.running_job = None
 
     def __str__(self) -> str:
         return f'Apache Hadoop Scheduler: {self.hostname}:{self.port}'
+
+    def get_job_list(self) -> List[Job]:
+        '''
+        Get the list of jobs from the Apache Hadoop scheduler
+
+        As Apache Hadoop does not have a scheduler, this method will return the actual running job, if any.
+
+        '''
+        return [self.running_job]
