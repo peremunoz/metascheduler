0f2ad35719b962bf088eee779fd865c619eb07a9
peremunoz <peremunozfiguerol33@gmail.com>
2024-05-26 14:13:29 +0200

    Implement the get cluster mode command

--- client/subcommands/get.py
+++ client/subcommands/get.py
@@ -1,13 +1,23 @@
+from requests import Response
 import typer
-import os
+from rich import print, print_json
+from rich.panel import Panel
+
+from client.helpers.http_client import HTTP_Client
 
 app = typer.Typer(no_args_is_help=True)
 
 
 @app.command()
 def cluster_mode():
-    print(
-        f"Getting cluster mode from {os.getenv('API_IP')}:{os.getenv('API_PORT')}")
+    response: Response = HTTP_Client().get('/cluster/mode')
+    cluster_mode = response.json()
+    panel = Panel(
+        f"[bold cyan]Cluster Mode:[/bold cyan]\n{cluster_mode}",
+        title="[bold magenta]Cluster Information[/bold magenta]",
+        border_style="green"
+    )
+    print(panel)
 
 
 if __name__ == "__main__":
