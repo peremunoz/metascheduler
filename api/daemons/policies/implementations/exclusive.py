9ccd9a1b6e2f09bcd3a60c8993b355348be49af1
peremunoz <peremunozfiguerol33@gmail.com>
2024-06-10 21:30:29 +0200

    feat: Implement exclusive planification policy
    Closes #38

--- api/daemons/policies/implementations/exclusive.py
+++ api/daemons/policies/implementations/exclusive.py
@@ -1,6 +1,6 @@
 from typing import List
 from api.daemons.policies.planification_policy import PlanificationPolicy
-from api.interfaces.scheduler import Scheduler
+from api.interfaces.job import Job
 
 
 class ExclusivePolicy(PlanificationPolicy):
@@ -16,8 +16,22 @@ class ExclusivePolicy(PlanificationPolicy):
 
     def __init__(self, policy: PlanificationPolicy):
         ''' Initialize the Exclusive Policy '''
-        super().__init__(policy.schedulers)
+        super().__init__(policy.schedulers, policy.highest_priority)
 
-    def apply(self, metascheduler_queue: List[Scheduler]):
+    def apply(self, to_be_queued_jobs: List[Job]):
         ''' Apply the Exclusive Policy '''
-        pass
+        if not to_be_queued_jobs:
+            print('No jobs to be queued.')
+            return
+
+        next_job = to_be_queued_jobs[0]
+        next_job_scheduler_index = next_job.queue - 1
+
+        for i, scheduler in enumerate(self.schedulers):
+            if i != next_job_scheduler_index and len(scheduler.get_job_list()) > 0:
+                print(
+                    f'Scheduler {i} has running jobs. Cannot run job from scheduler {next_job_scheduler_index}.')
+                return
+        print(
+            f'Queuing job {next_job.id_} from scheduler {next_job_scheduler_index}...')
+        self.schedulers[next_job_scheduler_index].queue_job(next_job)
