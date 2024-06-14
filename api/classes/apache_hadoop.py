ad88688c81bdc6cba528f585412c1a1e81ef700b
peremunoz <peremunozfiguerol33@gmail.com>
2024-06-14 22:02:48 +0200

    Implement get all jobs info for hadoop

--- api/classes/apache_hadoop.py
+++ api/classes/apache_hadoop.py
@@ -110,6 +110,30 @@ class ApacheHadoop(Scheduler):
                     continue
                 node.send_command(f'renice {new_nice} {pid}')
 
+    def get_all_jobs_info(self) -> List[Tuple[int, int, float, float]]:
+        '''
+        Get the information of all running jobs
+
+        '''
+        node = self.master_node
+        ps_output = node.send_command(f'ps -eo pid,comm,nice,%cpu,%mem')
+        return self._get_job_info_from_ps(ps_output)
+
+    def _get_job_info_from_ps(self, ps_output: str) -> List[Tuple[int, int, float, float]]:
+        '''
+        Get the list of processes of the running jobs.
+
+        Search for the sge_shepherd process and get the PID of the process and the nice value.
+
+        '''
+        job_processes_pid_nice_cpu_mem = []
+        lines = ps_output.split('\n')
+        for line in lines:
+            if 'java' in line:
+                job_processes_pid_nice_cpu_mem.append(
+                    (int(line.split()[0]), int(line.split()[2]), float(line.split()[3]), float(line.split()[4])))
+        return job_processes_pid_nice_cpu_mem
+
     def _get_job_processes_from_ps(self, ps_output: str) -> Tuple[int, int]:
         job_processes_pid_nice = []
         lines = ps_output.split('\n')
